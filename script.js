286692b06b9875366a702a6203413fe8b6aded19
MargitaRahimi <mr58700@ubt-uni.net>
2022-12-25 00:56:51 +0100

    changes

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
-var counter =1;
+/*var counter =1;
 setInterval(function(){
     document.getElementById('radio' + counter).checked=true;
     counter++;
     if(counter>3){
         counter=1;
     }
-},5000);
+},5000); */
+
+
+function valido(){
+    var username=document.getElementById('usernameInput').value;
+    var usernameRegex=/^[A-Za-z]{5,}[0-9]{1,}$/
+    var password=document.getElementById('passwordInput').value;
+    var passwordRegex=/^[A-Za-z]{5,}[0-9]{1,}$/
+
+    if(usernameRegex.test(username)){
+        alert("Username is valid");
+    }else{
+        alert("Username is not valid, it should contain capital letters, at least 5 letters, and a number!");
+    }
+    if(passwordRegex.test(password)){
+        alert("Password is valid");
+    }else{
+        alert("Password is not valid, it should contain 1 capital letters, at least 5 letters, and a number!")
+    }
+    
+}
+
+
