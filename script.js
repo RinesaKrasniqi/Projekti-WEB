be8d99b7a5d78350b58bdfd3cfa804f30089b393
MargitaRahimi <mr58700@ubt-uni.net>
2022-12-25 13:23:21 +0100

    changes3

--- script.js
+++ script.js
@@ -1,13 +1,57 @@
-/*var counter =1;
+/*var array =[
+    'C:/Users/Lenovo/OneDrive/Desktop/Projekt/Changes/Images/dentist-patient-kid.jpg',
+    'C:/Users/Lenovo/OneDrive/Desktop/Projekt/Changes/Images/dentist-patient.jpg',
+    'C:/Users/Lenovo/OneDrive/Desktop/Projekt/Changes/Images/dental-office.png'
+    
+    
+   
+];
+​
+​
+var index=0;
+var koha=1000;
+function krijoSlider(){
+   document.getElementById('imazhi').src=array[index];
+   index++;
+   if(index==array.length){
+       index=0;
+   }
+​
+   setTimeout("krijoSlider()",koha);
+​
+}
+krijoSlider();*/
+​
+var counter =1;
 setInterval(function(){
     document.getElementById('radio' + counter).checked=true;
     counter++;
     if(counter>3){
         counter=1;
     }
-},5000); */
-
-
+},2000); 
+​
+​
+function valido(){
+    var username=document.getElementById('usernameInput').value;
+    var usernameRegex=/^[A-Za-z]{5,}[0-9]{1,}$/
+    var password=document.getElementById('passwordInput').value;
+    var passwordRegex=/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{5,}$/
+​
+    if(usernameRegex.test(username)){
+        
+    }else{
+        alert("This username doesn't exist, check again!");
+    }
+    if(passwordRegex.test(password)){
+        
+    }else{
+        alert("Password doesn't exit, try again!");
+    }
+    
+}
+​
+​
 function validoSignUp(){
     var emri=document.getElementById("name").value;
     var emriregex=/^[A-Z][a-z]{3,15}\s[A-Z][a-z]{3,15}$/
@@ -16,60 +60,66 @@ function validoSignUp(){
     var user=document.getElementById("usernameInput").value;
     var userregex=/^[A-Z][a-z0-9]{3,15}$/
     var password=document.getElementById("passwordInput").value;
-    var passregex=/^[A-Za-z0-9]{5,15}$/
+    var passregex=/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{5,}$/
+​
     if(emriregex.test(emri)){
-       alert("name is valid");
+       
     }else{
        alert("name should have 1 capital letter to the name, a space, and a capital letter for surname!");
     }
     if(emailregex.test(email)){
-       alert("email is valid");
+       
     }else{
        alert("email should be in this format: name.surname@---.com/edu");
     }
     if(userregex.test(user)){
-       alert("Username is valid");
+       
     }else{
-       alert("Username should have 1 capital letter and a number!");
+       alert("Username should have first letter a capital letter and 3 letters!");
     }
     if(passregex.test(password)){
-       alert("password is valid");
+       
     }else{
-       alert("Password should have 5 or more letters!");
+       alert("Enter password again, it should contain minimum eight characters, at least one letter and one number!");
     }
+​
+​
 }
+​
 function validoContact(){
   var emri=document.getElementById("Name-1").value;
   var emriregex=/^[A-Z][a-z]{3,15}\s[A-Z][a-z]{3,15}$/
   var email=document.getElementById("Email-1").value;
   var emailregex=/[a-z]+@([a-z]+\.)+(edu|com)/
   var phone=document.getElementById("Phone-Number-1").value;
-  var phoneregex=/^[0-9]{3}[0-9]{3}[0-9]{3}$/
+  var phoneregex=/^[0-9]{3}-[0-9]{3}-[0-9]{3}$/
   var date=document.getElementById("datepicker").value;
-  var dateregex=/^[0-9]\/{2}[0-9]{2}\/[0-9]{4}$/
+  var dateregex=/^((?:19|20)[0-9][0-9])-(0?[1-9]|1[012])-(0?[1-9]|[12][0-9]|3[01])$/
+  
+​
   if(emriregex.test(emri)){
-     alert("name is valid");
+     
   }else{
      alert("name should have 1 capital letter to the name, a space, and a capital letter for surname!");
   }
   if(emailregex.test(email)){
-     alert("email is valid");
+     
   }else{
      alert("email should be in this format: name.surname@---.com/edu");
   }
   if(phoneregex.test(phone)){
+     
   }else{
      alert("phone number should be in this format: 000-000-000!");
   }
-  if(phone.length<9){
-     alert("Phone length should be 9 digits!");
-  }
+  
   if(dateregex.test(date)){
-     alert("date is valid!");
+     
   }else{
-     alert("Date should be in this format: mm/dd/yyyy");
+     alert("There are no free appointments for that day, try another one!");
   }
-  if(date.slice(-4)>2022 || date.slice(-4)<2022){
-     alert("This year isnt valid!");
-  }
+  
+​
+  
+  
 }
